e4f2655ccc04bdd1c5bc010b6ff254f74c27411a
Ant178 <86801514+Ant178@users.noreply.github.com>
2022-07-03 17:18:07 -0500

    Add files via upload

---
+++ Numeros_conteo.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+auto main()->int{
+    short int a,b;
+    int i;
+    cin>>a>>b;
+    for(i=a;i<=b;i++){
+        cout<<a<<" ";
+        ++a;
+        }
+    return 0;
+}
